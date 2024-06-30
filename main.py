ac89ad81197667af9a41654da2374a4bf7e6d03e
aryan <kanadaaryan9@gmail.com>
2024-06-30 09:56:38 +0530

    Add table

--- main.py
+++ main.py
@@ -13,10 +13,31 @@ for filepath in filepaths:
     filename = filename0.split(" ")[1]
     invoices_nr, date = filename.split("-")
     pdf.set_font(family="Times",style="B",size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice no: {invoices_nr}", align="L",ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice no: {invoices_nr}", align="L",ln =1)
     pdf.set_font(family="Times",style="B",size=16)
     pdf.cell(w=50, h=8, txt=f"Date: {date}", align="L",ln=1)
-    pdf.output(f"PDFs/{filename}.pdf")
+    #For header
+    column = df.columns
+    column = [item.replace("_"," ").title() for item in column]
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=column[0], border=1)
+    pdf.cell(w=35, h=8, txt=column[1], border=1)
+    pdf.cell(w=35, h=8, txt=column[2], border=1)
+    pdf.cell(w=30, h=8, txt=column[3], border=1)
+    pdf.cell(w=30, h=8, txt=column[4], border=1,ln=1)
 
 
+    #for row
+    for index, row in df.iterrows():
+
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]),border=1)
+        pdf.cell(w=35, h=8, txt=str(row["product_name"]),border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1,ln=1)
+        pdf.output(f"PDFs/{filename}.pdf")
 
